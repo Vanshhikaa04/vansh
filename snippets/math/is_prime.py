3e63e57a68b2bc237c6d619d255578438acb6e87
Vanshhikaa04 <268219499+Vanshhikaa04@users.noreply.github.com>
2026-08-15 11:19:29 +0000

    Add is_prime to math snippets

---
+++ snippets/math/is_prime.py
@@ -0,0 +1,16 @@
+"""Check whether a positive integer is prime."""
+
+
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    if n in (2, 3):
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
